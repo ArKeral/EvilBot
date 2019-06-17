e26884f1f6186ce4cb76efbca1a2626ccf23a980
kogayra <ra.kogay@tensor.ru>
2019-06-17 08:48:20 +0300

    бот

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
-import telebot;
-bot = telebot.TeleBot('891787770:AAFqRNEuDZSQuoN1qJUwF8B5gLyr_Sej9CE');
+import telebot
+bot = telebot.TeleBot('813537832:AAHv4gM-545wNaGAcukzlUlYb57do1AT-2Y')
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(func=lambda message: True)
 def get_text_messages(message):
-    if message.text.find("толстый") !=-1:
-        bot.send_message(message.from_user.id, "Сам жрешь много!!!!")
+    if message.text.lower().find("толстый") != -1:
+        bot.send_message(message.chat.id, "Сам жрешь много!!!!")
 
-    if message.text.find("але") != -1:
-        bot.send_message(message.from_user.id, "Иди на хуй со своим алебежем!!!")
+    elif message.text.lower().find("але") != -1:
+        bot.send_message(message.chat.id, "Иди на хуй со своим алебежем!!!")
 
-    if message.text.find("отел") != -1:
-        bot.send_message(message.from_user.id, "Иди на хуй со своим hotel'ежем!!!")
-
-
-    bot.send_message(message.from_user.id, "Иди на хуй")
+    elif message.text.lower().find("отел") != -1:
+        bot.send_message(message.chat.id, "Иди на хуй со своим hotel'ежем!!!")
+    else:
+        bot.send_message(message.chat.id, "Иди на хуй")
 
 
 bot.polling(none_stop=True, interval=0)
-
