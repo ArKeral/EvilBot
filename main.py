8aef6c1f7607e37a083f3855e747692c47693b07
kogayra <ra.kogay@tensor.ru>
2019-06-17 08:19:26 +0300

    бот

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import telebot;
 bot = telebot.TeleBot('891787770:AAFqRNEuDZSQuoN1qJUwF8B5gLyr_Sej9CE');
 
+
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
+    if message.text.find("толстый") !=-1:
+        bot.send_message(message.from_user.id, "Сам жрешь много!!!!")
+
+    if message.text.find("але") != -1:
+        bot.send_message(message.from_user.id, "Иди на хуй со своим алебежем!!!")
+
+    if message.text.find("отел") != -1:
+        bot.send_message(message.from_user.id, "Иди на хуй со своим hotel'ежем!!!")
+
+
     bot.send_message(message.from_user.id, "Иди на хуй")
 
 
